6caea5725bc91f255d42cab6658db71bd4ebb292
Stefan Adolf <stadolf@gmail.com>
2021-12-25 23:30:55 +0100

    adds login with google & github adds chakra
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  config: {
+    //initialColorMode: "dark",
+    useSystemColorMode: true
+  },
+})
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return <ChakraProvider theme={theme}>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  </ChakraProvider>
 }
 
 export default MyApp
