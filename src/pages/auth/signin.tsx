9d898cb04120a3cbf83f08682914394c7a6fcc7b
Stefan Adolf <stadolf@gmail.com>
2022-02-23 15:49:09 +0100

    fixes callbackurl on custom signin pages
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -7,20 +7,26 @@ import {
   Input,
   Text,
 } from "@chakra-ui/react";
-import { Provider } from "next-auth/providers";
+import {
+  Provider,
+  OAuthProvider,
+  CredentialsProvider,
+} from "next-auth/providers";
 import { getCsrfToken, getProviders, signIn } from "next-auth/react";
 
 export function CredentialSignin({
   provider,
   csrfToken,
+  callbackUrl,
 }: {
   provider: Provider;
   csrfToken: string;
+  callbackUrl: string;
 }) {
   return (
     <form method="post" action="/api/auth/callback/credentials">
       <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-
+      <input name="callbackUrl" type="hidden" defaultValue={callbackUrl} />
       <FormLabel>
         Email
         <Input name="email" type="email" />
@@ -37,9 +43,11 @@ export function CredentialSignin({
 export default function SignIn({
   providers,
   csrfToken,
+  callbackUrl,
 }: {
   providers: Provider[];
   csrfToken: string;
+  callbackUrl: string;
 }) {
   const credentialProvider = Object.values(providers).find(
     (p) => p.type === "credentials"
@@ -48,12 +56,11 @@ export default function SignIn({
   return (
     <Flex direction="column" gridGap={3} my={5}>
       <Heading textTransform="uppercase">Signin</Heading>
-
       {Object.values(providers)
         .filter((p) => p.type === "oauth")
         .map((provider: Provider) => (
           <Flex key={provider.name}>
-            <Button onClick={() => signIn(provider.id)}>
+            <Button onClick={() => signIn(provider.id, { callbackUrl })}>
               Sign in with {provider.name}
             </Button>
           </Flex>
@@ -65,19 +72,23 @@ export default function SignIn({
         <Divider orientation="horizontal" />
       </Flex>
       {credentialProvider && (
-        <CredentialSignin provider={credentialProvider} csrfToken={csrfToken} />
+        <CredentialSignin
+          provider={credentialProvider}
+          csrfToken={csrfToken}
+          callbackUrl={callbackUrl}
+        />
       )}
     </Flex>
   );
 }
 
-// This is the recommended way for Next.js 9.3 or newer
 export async function getServerSideProps(context) {
   const providers = await getProviders();
   return {
     props: {
       providers,
       csrfToken: await getCsrfToken(context),
+      callbackUrl: context.query.callbackUrl,
     },
   };
 }
