bd0a0c3cb4f33d05029a6695bdb5ba1d9aa509a3
Stefan Adolf <stadolf@gmail.com>
2022-04-13 10:25:49 +0200

    user / account creation on siwe
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -2,40 +2,63 @@ import { Button, Flex, Spacer, Text } from "@chakra-ui/react";
 import { signIn, useSession, getCsrfToken } from "next-auth/react";
 import { useAccount, useConnect, useNetwork, useSignMessage } from "wagmi";
 import { SiweMessage } from "siwe";
+import { useCallback } from "react";
 
 export default function LoginComponent() {
   const { data: session } = useSession();
-  const [{ data: connectData, error }, connect] = useConnect();
+  const [{ data: connectData, loading }, connect] = useConnect();
   const [{ data: networkData }] = useNetwork();
   const [{ data: accountData }] = useAccount();
   const [, signMessage] = useSignMessage();
 
-  const handleSiwe = async () => {
+  const handleSiwe = useCallback(async () => {
     try {
-      await connect(connectData.connectors[0]);
+      const csrfToken = await getCsrfToken();
+      console.log(`got csrf token ${csrfToken}`, connectData.connectors);
+      console.log("connector", loading, connectData.connectors[0]);
+      //const result = await connect(connectData.connectors[0]);
+      //console.debug(result);
+
+      // if (result.error) {
+      //   console.error(result.error);
+      //   return;
+      // }
+
       const callbackUrl = "/";
       const message = new SiweMessage({
         domain: window.location.host,
         address: accountData?.address,
-        statement: "Sign in with Ethereum to the app.",
+        statement: "Sign into The Takeover with an Ethereum wallet.",
         uri: window.location.origin,
         version: "1",
-        chainId: networkData?.chain?.id,
-        nonce: await getCsrfToken(),
+        chainId: networkData.chain?.id,
+        nonce: csrfToken,
       });
+
       const { data: signature, error } = await signMessage({
         message: message.prepareMessage(),
       });
-      signIn("credentials", {
-        message: JSON.stringify(message),
-        redirect: false,
-        signature,
-        callbackUrl,
-      });
+
+      if (signature) {
+        await signIn("ethereum", {
+          message: JSON.stringify(message),
+          redirect: false,
+          signature,
+          callbackUrl,
+        });
+      }
     } catch (error) {
       window.alert(error);
     }
-  };
+  }, [
+    session,
+    signMessage,
+    loading,
+    networkData,
+    accountData,
+    connect,
+    connectData,
+  ]);
 
   if (session) {
     return (
@@ -44,13 +67,14 @@ export default function LoginComponent() {
         <Text fontSize="sm" fontWeight="bold" isTruncated>
           {session.user?.email}
         </Text>
+        <Button onClick={handleSiwe}>Sign in with Ethereum</Button>
       </Flex>
     );
   }
   return (
     <Flex>
       <Button onClick={() => signIn()}>Sign in</Button>
-      <Button onClick={() => handleSiwe()}>Sign in with Ethereum</Button>
+      <Button onClick={handleSiwe}>Sign in with Ethereum</Button>
     </Flex>
   );
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,65 +1,28 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { prismaAdapter } from "../../../modules/api/adapter";
+import { adapterClient, prismaAdapter } from "../../../modules/api/adapter";
 import { emailProvider } from "../../../modules/api/emailProvider";
-import { SiweMessage } from "siwe";
-import { getCsrfToken } from "next-auth/react";
+import { ethereumProvider } from "../../../modules/api/ethereumProvider";
 
 //https://github.com/spruceid/siwe-next-auth-example/blob/main/pages/api/auth/%5B...nextauth%5D.ts
 
-export default NextAuth({
-  adapter: prismaAdapter,
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+const defaultProviders = [
+  GithubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  }),
+];
+
+export default async function auth(req, res) {
+  const providers = [
+    ...defaultProviders,
     emailProvider,
-
-    CredentialsProvider({
-      name: "Ethereum",
-      id: "ethereum",
-      credentials: {
-        message: {
-          label: "Message",
-          type: "text",
-          placeholder: "0x0",
-        },
-        signature: {
-          label: "Signature",
-          type: "text",
-          placeholder: "0x0",
-        },
-      },
-      async authorize(credentials) {
-        try {
-          const siwe = new SiweMessage(
-            JSON.parse(credentials?.message || "{}")
-          );
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL as string);
-          if (siwe.domain !== nextAuthUrl.host) {
-            return null;
-          }
-
-          if (siwe.nonce !== (await getCsrfToken({ req }))) {
-            return null;
-          }
-
-          await siwe.validate(credentials?.signature || "");
-          return {
-            id: siwe.address,
-          };
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
+    ethereumProvider(req),
 
     // CredentialsProvider({
     //   // The name to display on the sign in form (e.g. 'Sign in with...')
@@ -94,38 +57,52 @@ export default NextAuth({
     //     return user;
     //   },
     // }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    session: async ({ session, token }) => {
-      if (session?.user) {
-        session.user.id = token.uid;
-      }
-      return session;
+  ];
+
+  return await NextAuth(req, res, {
+    adapter: prismaAdapter,
+    providers,
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+      jwt: async ({ user, token }) => {
+        console.debug("jwt", user, token);
+        if (user) {
+          token.uid = user.id;
+          const ethAccount = await adapterClient.account.findFirst({
+            where: {
+              userId: user?.id,
+              provider: "ethereum",
+            },
+          });
+          token.eth = ethAccount?.providerAccountId;
+        }
+        return token;
+      },
+      session: async ({ session, token, user }) => {
+        if (session?.user) {
+          session.user.id = token.uid;
+          session.eth = token.eth;
+        }
+        return session;
+      },
+      redirect({ url, baseUrl }) {
+        if (url.startsWith(baseUrl)) return url;
+        // Allows relative callback URLs
+        else if (url.startsWith("/")) return new URL(url, baseUrl).toString();
+        return baseUrl;
+      },
     },
-    jwt: async ({ user, token }) => {
-      if (user) {
-        token.uid = user.id;
-      }
-      return token;
+    session: {
+      strategy: "jwt",
     },
-    redirect({ url, baseUrl }) {
-      if (url.startsWith(baseUrl)) return url;
-      // Allows relative callback URLs
-      else if (url.startsWith("/")) return new URL(url, baseUrl).toString();
-      return baseUrl;
+    pages: {
+      signIn: "/auth/signin",
     },
-  },
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
-  // callbacks: {
-  //   async signIn({ user, account, profile, email, credentials }) {
-  //     console.log(user, account, profile, email, credentials);
-  //     return true;
-  //   },
-  // },
-});
+    // callbacks: {
+    //   async signIn({ user, account, profile, email, credentials }) {
+    //     console.log(user, account, profile, email, credentials);
+    //     return true;
+    //   },
+    // },
+  });
+}
