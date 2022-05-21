7704dd17f8f488a9e171314ebd8b40e7451e15b6
Stefan <stadolf@gmail.com>
2022-05-21 21:50:13 +0200

    extends display for desktop
    Signed-off-by: Stefan <stadolf@gmail.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,20 +7,24 @@ export default function Layout(props: { children: React.ReactNode }) {
   const { colorMode } = useColorMode();
 
   return (
-    <Container
-      maxW="container.md"
-      p="0"
-      h="100vh"
-      as={Flex}
-      direction="column"
-      pl={2}
-      pr={2}
-      backgroundRepeat="no-repeat"
+    <Flex
+      backgroundAttachment="fixed"
+      backgroundRepeat="repeat-x"
       backgroundImage={colorMode == "dark" ? bgpoly.src : "none"}
       backgroundPosition={["20% -15%", "20% -25%", "20% -50%"]}
     >
-      <Navbar />
-      {children}
-    </Container>
+      <Container
+        maxW="container.md"
+        p="0"
+        minH="100vh"
+        as={Flex}
+        direction="column"
+        pl={2}
+        pr={2}
+      >
+        <Navbar />
+        {children}
+      </Container>
+    </Flex>
   );
 }
