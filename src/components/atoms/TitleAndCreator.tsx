4a83702e68e6a5cd0d85d452c5661f4c37c963db
Stefan Adolf <stadolf@gmail.com>
2022-05-30 12:54:59 +0200

    improves bundle view shuts off titles on embeds after 2 seconds
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- src/components/atoms/TitleAndCreator.tsx
+++ src/components/atoms/TitleAndCreator.tsx
@@ -2,14 +2,27 @@ import { Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 import { XLink } from "../../types/Link";
 
-export const TitleAndCreator = (props: { link: XLink; color?: string }) => {
-  const { link, color } = props;
+export const TitleAndCreator = (props: {
+  link: XLink;
+  color?: string;
+  backdrop?: boolean;
+}) => {
+  const { link, color, backdrop } = props;
+  let backdropProps;
+  if (backdrop)
+    backdropProps = {
+      bg: "black",
+      px: 6,
+      py: 2,
+    };
+  else backdropProps = {};
+
   return (
     <Flex direction="column" align="center">
-      <Heading size="lg" color={color}>
+      <Heading size="lg" color={color} {...backdropProps}>
         {link.creator.name}
       </Heading>
-      <Heading size="md" color={color}>
+      <Heading size="md" color={color} {...backdropProps}>
         {link.metadata.title}
       </Heading>
     </Flex>
