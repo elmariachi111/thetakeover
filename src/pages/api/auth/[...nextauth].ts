51d10b59ca9925aa5ffcd67c06a0d8a5fc9da4f3
Stefan <stadolf@gmail.com>
2022-07-14 00:25:27 +0200

    updates dependencies
    Signed-off-by: Stefan <stadolf@gmail.com>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,8 +10,8 @@ import { ethereumProvider } from "../../../modules/api/ethereumProvider";
 
 const defaultProviders = [
   GithubProvider({
-    clientId: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
+    clientId: process.env.GITHUB_ID as string,
+    clientSecret: process.env.GITHUB_SECRET as string,
   }),
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
